9bc5502528b0a75e81414ffcb607979c4ccf8802
Meblom <xuniqh@gmail.com>
2017-10-18 22:44:17 +0200

    Added AverageSpeedInMiles Class to ChapterOne.ProgrammingExercises Added PopulationProjection Class to ChapterOne.ProgrammingExercises Added AlgebraSolveLinearEquations Class To ChapterOne.ProgramExercises Added AverageSpeedInKilometers Class To ChapterOne.ProgramExercises

--- src/ChapterOne/ProgrammingExercises/AverageSpeedInKilometers.java
+++ src/ChapterOne/ProgrammingExercises/AverageSpeedInKilometers.java
@@ -1,4 +1,13 @@
 package ChapterOne.ProgrammingExercises;
 
 public class AverageSpeedInKilometers {
+    public static void main(String[] args) {
+        System.out.println("Distance = 24 miles =" + (24.0 * 1.6) + " kilometers\n");
+        System.out.println("Time = 1 hour 40 minutes 35 seconds" + " = "
+                + (1 + (((40.0 * 60.0) + 35.0) / 3600)) + " hours.\n");
+        System.out.print("Average Speed In kilometers per hour = ");
+        System.out.println(((24.0 * 1.6) / (1 + (((40.0 * 60.0) + 35.0) / 3600)))
+                + " kph.");
+    }
 }
+
