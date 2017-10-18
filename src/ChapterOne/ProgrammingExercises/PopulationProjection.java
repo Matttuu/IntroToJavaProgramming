3afc4fbe335f284e5973441b1c19d3b8cf2029f3
Meblom <xuniqh@gmail.com>
2017-10-18 22:13:09 +0200

    Added AverageSpeedInMiles Class to ChapterOne.ProgrammingExercises Added PopulationProjection Class to ChapterOne.ProgrammingExercises

--- src/ChapterOne/ProgrammingExercises/PopulationProjection.java
+++ src/ChapterOne/ProgrammingExercises/PopulationProjection.java
@@ -1,5 +1,21 @@
 package ChapterOne.ProgrammingExercises;
 
 public class PopulationProjection {
+    public static void main(String[] args) {
+
+        int birth = 7;
+        int death = 13;
+        int newImmigrant = 45;
+        int currentPopulation = 312032486;
+        int year = 365;
+        int yearlyBirths = (((60 * 60 * 24 * year) / birth));
+        int yearlyDeaths = (((60 * 60 * 24 * year) / death));
+        int yearlyImmigrant = (((60 * 60 * 24 * year) / newImmigrant));
+        int yearlyPopulation = (yearlyBirths + newImmigrant - yearlyDeaths);
+
+        
+
+
+    }
 
 }
