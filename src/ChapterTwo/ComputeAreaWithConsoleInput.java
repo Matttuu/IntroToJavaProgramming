800550ff06b29b1fd32e9d4a8761e437e823c8e2
Meblom <xuniqh@gmail.com>
2017-10-18 23:08:52 +0200

    Added AverageSpeedInMiles Class to ChapterOne.ProgrammingExercises Added PopulationProjection Class to ChapterOne.ProgrammingExercises Added AlgebraSolveLinearEquations Class To ChapterOne.ProgramExercises Added AverageSpeedInKilometers Class To ChapterOne.ProgramExercises

---
+++ src/ChapterTwo/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,20 @@
+package ChapterTwo;
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args) {
+        // Create a scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a radius
+        System.out.println("Enter a number for radius: ");
+        double radius = input.nextDouble();
+
+        // Compute area
+        double area = radius * radius * 3.14159;
+        System.out.println("The area for the circle of radius " +
+            radius  + " is " + area);
+
+    }
+}
